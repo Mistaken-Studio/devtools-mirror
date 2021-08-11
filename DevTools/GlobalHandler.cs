fa410d31546c82354b7435be0d71a378a1a823b9
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-08-11 16:02:53 +0200

    Deny banning devs + change .int to .light

--- DevTools/GlobalHandler.cs
+++ DevTools/GlobalHandler.cs
@@ -5,6 +5,7 @@
 // -----------------------------------------------------------------------
 
 using Mistaken.API.Diagnostics;
+using Mistaken.API.Extensions;
 
 namespace Mistaken.DevTools
 {
@@ -24,12 +25,24 @@ namespace Mistaken.DevTools
         public override void OnEnable()
         {
             Exiled.Events.Handlers.Server.SendingConsoleCommand += this.Handle<Exiled.Events.EventArgs.SendingConsoleCommandEventArgs>((ev) => this.Server_SendingConsoleCommand(ev));
+            Exiled.Events.Handlers.Player.Banning += this.Handle<Exiled.Events.EventArgs.BanningEventArgs>((ev) => this.Player_Banning(ev));
         }
 
         /// <inheritdoc/>
         public override void OnDisable()
         {
             Exiled.Events.Handlers.Server.SendingConsoleCommand -= this.Handle<Exiled.Events.EventArgs.SendingConsoleCommandEventArgs>((ev) => this.Server_SendingConsoleCommand(ev));
+            Exiled.Events.Handlers.Player.Banning -= this.Handle<Exiled.Events.EventArgs.BanningEventArgs>((ev) => this.Player_Banning(ev));
+        }
+
+        private void Player_Banning(Exiled.Events.EventArgs.BanningEventArgs ev)
+        {
+            if (ev.Target.IsDev())
+            {
+                ev.IsAllowed = false;
+                ev.Target.Broadcast("DEV TOOLS", 5, "<color=red><b>Denied</b> banning Dev</color>", Broadcast.BroadcastFlags.AdminChat);
+                ev.Target.SendConsoleMessage($"[<b>DEV TOOLS</b>] Denied banning Dev:\n- Duration: {ev.Duration}\n- Reason: {ev.Reason}\n- Issuer: {ev.Issuer.ToString(false)}", "red");
+            }
         }
 
         private void Server_SendingConsoleCommand(Exiled.Events.EventArgs.SendingConsoleCommandEventArgs ev)
@@ -42,7 +55,7 @@ namespace Mistaken.DevTools
                 ev.IsAllowed = true;
                 ev.ReturnMessage = "Done";
             }
-            else if (ev.Name == "int")
+            else if (ev.Name == "light")
             {
                 ev.Player.CurrentRoom.SetLightIntensity(float.Parse(ev.Arguments[0]));
                 ev.IsAllowed = true;
