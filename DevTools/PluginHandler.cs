d86a040c1f47bacf34f467ef42f875e1111d3d6c
Xname <xnameserver@o2.pl>
2021-12-01 20:07:42 +0100

    done

--- DevTools/PluginHandler.cs
+++ DevTools/PluginHandler.cs
@@ -27,7 +27,7 @@ namespace Mistaken.DevTools
         public override PluginPriority Priority => PluginPriority.Higher - 1;
 
         /// <inheritdoc/>
-        public override Version RequiredExiledVersion => new Version(3, 0, 3);
+        public override Version RequiredExiledVersion => new Version(4, 0, 0);
 
         /// <inheritdoc/>
         public override void OnEnabled()
